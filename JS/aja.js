c0ffe0a1aaf4f4761e43447b84c93bcc113aa2a5
Hurcam17 <43282664+Hurcam17@users.noreply.github.com>
2018-09-15 00:29:17 -0500

    Mayra
    Mayra!!

---
+++ JS/aja.js
@@ -0,0 +1,23 @@
+$(function () {
+	$("#ini").click(function () {
+		$("#presentar").load("inicio/inicio.html")
+		// body...
+	});
+	$("#vis").click(function () {
+		$("#presentar").load("Vision/vision.html")
+		// body...
+	});
+			// body...
+	$("#mis").click(function () {
+		$("#presentar").load("Mision/mision.html")
+		// body...
+			});
+	$("#int").click(function () {
+		$("#presentar").load("Integrantes/integrantes.html")
+		// body...
+	});
+	$("#video").click(function () {
+		$("#presentar").load("Video/video.html")
+		// body...
+	});
+})
